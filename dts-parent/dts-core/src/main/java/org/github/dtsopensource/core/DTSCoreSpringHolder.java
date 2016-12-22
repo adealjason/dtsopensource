028e1c120f58038357953e0fb71f6758c48ee679
leegaofeng <ligaofeng@zhongan.com>
2016-12-22 10:51:27 +0800

    first commit

--- dts-parent/dts-core/src/main/java/org/github/dtsopensource/core/DTSCoreSpringHolder.java
+++ dts-parent/dts-core/src/main/java/org/github/dtsopensource/core/DTSCoreSpringHolder.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2016 Zhongan.com All right reserved. This software is the
- * confidential and proprietary information of Zhongan.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Zhongan.com.
- */
 package org.github.dtsopensource.core;
 
 import org.springframework.beans.BeansException;
